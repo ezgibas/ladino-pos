0359ad0273f02a4c0ed725836b523ee5f7f42d15
ezgibas <basezgi@gmail.com>
2025-03-08 20:51:43 -0500

    create tokenization script

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -1,1 +1,17 @@
 # tokenize Ladino & Spanish data
+import spacy
+from spacy.lang.es.examples import sentences 
+
+# tokenizes one sentence using spaCy's Spanish tokenizer
+# and returns it as an array
+def tokenize(sentence, give_pos=False):
+    # load tokenizer
+    nlp = spacy.load("es_core_news_sm")
+    doc = nlp(sentence)
+    tokens = []
+    pos = []
+    for token in doc:
+        tokens.append(token)
+        if give_pos:
+            pos.append(token.pos_)
+    return tokens, pos
